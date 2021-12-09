de5c6f4bf9e458094ddf7595c3355123704b149a
cainpalmer <cain.a.palmer@gmail.com>
2021-12-09 18:56:03 -0500

    created index.js and server.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+
+// Variables
+const server = require('./api/server')
+const PORT = process.env.PORT || 5000
+
+// Listening
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
